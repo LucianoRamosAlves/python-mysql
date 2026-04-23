626b6ea146c560d371ffed1b9e5085beae6101ea
Luciano Ramos <lramosalves37@gmail.com>
2026-04-23 07:19:27 -0300

    creat db

---
+++ concepts/examples/hospital/db.py
@@ -0,0 +1,12 @@
+from dotenv  import load_dotenv
+import os
+import mysql.connector
+
+load_dotenv()
+
+def conectar():
+    return mysql.connector.connect(
+        host= os.getenv("DB_HOST"),
+        user= os.getenv("DB_USER"),
+        password= os.getenv("DB_PASSWORD"),
+    )
