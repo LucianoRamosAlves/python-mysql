d87660cd704c108ff44c6a3fe09f249a93cc34d2
Luciano Ramos <lramosalves37@gmail.com>
2026-04-23 07:58:08 -0300

    update app.py creat def criar_tabela_pacientes

--- concepts/examples/hospital/app.py
+++ concepts/examples/hospital/app.py
@@ -4,8 +4,31 @@ conexao = conectar()
 
 cursor = conexao.cursor()
 
-cursor.execute("CREATE DATABASE IF NOT EXISTS hospital")
-print("banco hospital criado com sucesso.")
+#? Banco de dados para um hospital
+def criar_banco(cursor): 
+    cursor.execute("CREATE DATABASE IF NOT EXISTS hospital")
+    print("banco hospital criado com sucesso.")
+
+# criar_banco(cursor)
+
+conexao.database = "hospital"
+
+
+
+
+def criar_tabela_pacientes(cursor):
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS pacientes (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+        idade INT NOT NULL,
+        email VARCHAR(100) NOT NULL UNIQUE
+    )""")
+    conexao.commit()
+    print("Tabela 'pacientes' criada com sucesso!")
+
+
+criar_tabela_pacientes(cursor)
 
 conexao.commit()
 
