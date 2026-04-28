b7fb5cb65859390d4ae6e3989d55f5cc45d5424a
Luciano Ramos <lramosalves37@gmail.com>
2026-04-28 06:52:58 -0300

    creat app.py

---
+++ concepts/examples/word/app.py
@@ -0,0 +1,16 @@
+from db import conectar #! 1 primeiro passo. Importar a função conectar() do módulo db.py para estabelecer uma conexão com o banco de dados MySQL.
+
+conexao = conectar() #! 2 segundo passo. Chamar a função conectar() para criar uma conexão com o servidor MySQL e armazenar essa conexão na variável conexao.
+cursor = conexao.cursor() #! 3 terceiro passo. Criar um cursor a partir da conexão. O cursor é um objeto que permite executar comandos SQL e interagir com o banco de dados.
+
+def criar_banco(cursor): #! 6 sexto passo. Definir uma função criar_banco() que recebe o cursor como argumento e executa um comando SQL para criar um banco de dados chamado "word" se ele ainda não existir. Após criar o banco, a função imprime uma mensagem de sucesso.
+    cursor.execute("CREATE DATABASE IF NOT EXISTS word") #! 7 sétimo passo. Executar o comando SQL para criar o banco de dados "word" usando o método execute() do cursor. O comando "CREATE DATABASE IF NOT EXISTS" garante que o banco só será criado se ele ainda não existir, evitando erros caso já exista um banco com esse nome.
+    conexao.commit() #! 8.1 passo. Confirmar a operação de criação do banco de dados usando conexao.commit(). Isso é necessário para garantir que as alterações sejam salvas no servidor MySQL.
+    print("banco word criado com sucesso.")
+
+criar_banco(cursor) #! 8 oitavo passo. Chamar a função criar_banco() passando o cursor como argumento para criar o banco de dados "word". Isso executará o comando SQL para criar o banco e imprimirá a mensagem de sucesso.
+
+conexao.database = "word" #! 9 nono passo. Selecionar o banco de dados "word" para que as próximas operações sejam realizadas dentro desse banco. Isso é feito atribuindo o nome do banco à propriedade database da conexão.
+
+cursor.close() #! 4 quarto passo. Fechar o cursor após criar o banco de dados, pois ele não é mais necessário para as próximas operações.
+conexao.close() #! 5 quinto passo. Fechar a conexão com o banco de dados para liberar recursos e evitar conexões desnecessárias.
